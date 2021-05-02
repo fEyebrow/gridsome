77c10eb595d47cdb4f4c006357efa428f397c0b9
四条眉毛 <fourfishxiake@gmail.com>
2021-05-02 15:24:14 +0800

    first

---
+++ gridsome.config.js
@@ -0,0 +1,42 @@
+// This is where project configuration and installed plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+module.exports = {
+  siteName: "Portfolio",
+  siteUrl: `https://fEyebrow.github.io`,
+  host: "0.0.0.0",
+  pathPrefix: '/gridsome',
+  titleTemplate: "%s - NWA",
+  siteDescription: "Creative technologist",
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "projects/**/*.md",
+        typeName: "ProjectPost",
+        resolveAbsolutePaths: true,
+        remark: {
+          externalLinksTarget: "_blank",
+          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+        }
+      }
+    },
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "journal/**/*.md",
+        typeName: "JournalPost",
+        resolveAbsolutePaths: true,
+        remark: {
+          externalLinksTarget: "_blank",
+          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+        }
+      }
+    }
+  ],
+  transformers: {
+    remark: {
+      plugins: ["@gridsome/remark-prismjs"]
+    }
+  }
+};
